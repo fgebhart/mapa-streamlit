5fc3b285d830b01a84fcf0d08de63a26dc0f2301
Fabian Gebhart <fabian.gebhart@gmail.com>
2022-03-17 13:25:44 +0100

    TST: work on adding a selenium test covering the actual functionality

--- tests/browser_tests/test_app.py
+++ tests/browser_tests/test_app.py
@@ -80,3 +80,43 @@ def test_streamlit_app__basic(live_server, webdriver) -> None:
 
     assert f"Made with mapa-streamlit v{__version__}" in about_text
     assert "Hi my name is" in about_text
+
+
+def test_streamlit_app__functionality(live_server, webdriver, take_screenshot) -> None:
+    assert webdriver.current_url == live_server.url
+    time.sleep(1)
+
+    # TODO verify that "CREATE STL" button is disabled
+    # TODO verify that "DOWNLOAD STL" button is disabled
+    # webdriver.implicitly_wait(3)
+    # click draw rect icon on folium map
+    take_screenshot(webdriver, "after_page_load")
+    webdriver.switch_to.frame(0)
+    rect_button = webdriver.find_element(By.CLASS_NAME, "leaflet-draw-draw-rectangle")
+    rect_button.click()
+
+    take_screenshot(webdriver, "clicked_rect_button")
+
+    # webdriver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(webdriver, 20).until(EC.vi
+    # sibility_of_element_located((By.XPATH, "//h2[text()='Interactive Choropleth Map']"))))
+    # WebDriverWait(webdriver, 3).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"div.css-6wrhc:nt
+    # h-child(3) > iframe:nth-child(1)")))
+    # take_screenshot(webdriver, "switched_to_iframe")
+    # elements = webdriver.find_elements_by_css_selector("svg.leaflet-zoom-animated>g path")
+    # for element in elements:
+    #     ActionChains(webdriver).move_to_element(element).perform()
+    #     print(WebDriverWait(webdriver, 3).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='info lea
+    # flet-control']"))).text)
+
+    leaflet_map = webdriver.find_element(By.ID, "map_div")
+    webdriver.switch_to.default_content()
+    time.sleep(1)
+    take_screenshot(webdriver, "switched_to_default_content")
+    draw_rect = ActionChains(webdriver).move_to_element_with_offset(leaflet_map, 20, 20)
+    draw_rect.click_and_hold()
+    draw_rect = ActionChains(webdriver).move_to_element_with_offset(leaflet_map, 200, 200)
+    draw_rect.release()
+    draw_rect.perform()
+
+    time.sleep(1)
+    breakpoint()
