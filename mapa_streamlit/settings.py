6e03357767c847c53421cb754ebfc4378537e5a6
Fabian Gebhart <fabian.gebhart@gmail.com>
2022-04-02 10:19:17 +0200

    ENH: add model size slider to customizations

--- mapa_streamlit/settings.py
+++ mapa_streamlit/settings.py
@@ -28,10 +28,11 @@ Made with mapa-streamlit v{__version__}
 DEFAULT_Z_OFFSET = 2
 DEFAULT_Z_SCALE = 2.0
 SQUARED_SIDE_RATIO = 1.0
+DEFAULT_MODEL_SIZE = 100
 
 
 class ZOffsetSlider:
-    label: str = "Z-offset"
+    label: str = "Z-offset:"
     min_value: int = 0
     max_value: int = 20
     value: int = DEFAULT_Z_OFFSET
@@ -39,12 +40,24 @@ class ZOffsetSlider:
 
 
 class ZScaleSlider:
-    label: str = "Z-scale"
-    min_value: int = 0.0
-    max_value: int = 5.0
-    value: int = DEFAULT_Z_SCALE
+    label: str = "Z-scale:"
+    min_value: float = 0.0
+    max_value: float = 5.0
+    value: float = DEFAULT_Z_SCALE
     step: float = 0.1
     help: str = "Factor to be multiplied to the z-axis in order to scale the elevation up (or down)."
+
+
+class ModelSizeSlider:
+    label: str = "Model size:"
+    min_value: int = 10
+    max_value: int = 200
+    value: int = DEFAULT_MODEL_SIZE
+    step: int = 1
+    help: str = (
+        "Desired output size of the 3d model in millimeter. In case of a rectangular shaped model, this value will be "
+        "used for the north-south dimension of the model."
+    )
 
 
 class SquaredCheckbox:
